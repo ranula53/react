e84ecdb37be5c2f1c90a47879697d89e827eb99b
ranula53 <ranula53@gmail.com>
2025-02-27 00:55:52 +0530

    partone

--- kdj-app/src/App.js
+++ kdj-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import partOne from './components/partOne';
 
 function App() {
   return (
     <div className="App">
       <h1>this is my first project</h1>
+      <partOne/>
     </div>
   );
 }
---
+++ kdj-app/src/components/partOne.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function partOne() {
+  return (
+    <div>
+      <h1>this is my first react component</h1>
+    </div>
+  )
+}
